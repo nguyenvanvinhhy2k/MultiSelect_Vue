1199399f9f6f97bae2bd8918d2841a940f5c9f55
Nguyễn Văn Vinh <86061869+nguyenvanvinhhy2k@users.noreply.github.com>
2022-04-25 15:21:54 +0700

    Update Multiselect.vue

--- src/components/Multiselect.vue
+++ src/components/Multiselect.vue
@@ -3,10 +3,10 @@
     <div class="Container">
       <input
         @click="handleClick"
-        style="margin-top: 55px;"
+        style="margin-top: 55px"
         type="text"
         placeholder="Chọn Thành Phố"
-        v-model="search"
+        @input="handleInput"
       />
       <span>
         <svg
@@ -21,43 +21,48 @@
           <path d="M8 0H0L4 4L8 0Z" fill="#666666" /></svg
       ></span>
       <div class="content" v-show="focused">
-      <div class="content_option" v-for="city in filterCitys" :key="city">
-        <input
-          class="content_checkbox checked"
-          type="checkbox"
-          @change="selectedCitys($event, city)"
-          :checked="selectCitys.some((item) => item == city.name)"
-        />
-        <div class="content_text">{{ city.name }}</div>
+        <div
+          class="content_option"
+          v-for="city in filterCitys"
+          :key="city.code"
+        >
+          <input
+            class="content_checkbox checked"
+            type="checkbox"
+            @change="selectedCitys($event, city)"
+            :checked="selectCitys.some((item) => item == city.name)"
+          />
+          <div class="content_text">{{ city.name }}</div>
+        </div>
       </div>
-    </div>
-    <div class="Containerr" v-if="selectCitys.length > 0">
-      <div
-        class="Container_text"
-        v-for="selectCity in selectCitys"
-        :key="selectCity"
-      >
-        <p class="">{{ selectCity }}</p>
-        <span @click="removeSelectCity(selectCity)"
-          ><img src="../assets/remove.jpg" alt=""
-        /></span>
+      <div class="Containerr" v-if="selectCitys.length > 0">
+        <div
+          class="Container_text"
+          v-for="selectCity in selectCitys"
+          :key="selectCity"
+        >
+          <p class="">{{ selectCity }}</p>
+          <span @click="removeSelectCity(selectCity)"
+            ><img src="../assets/remove.jpg" alt=""
+          /></span>
+        </div>
       </div>
-    </div>
-<div class="content_button" v-show="focused">
-      <button
-        :class="{ 'disabled-btn': selectCitys.length >= 0 }"
-        @click="focused = !focused"
-        class="button1"
-      >
-        Đồng ý
-      </button>
-      <button @click="focused = false" class="button2">Hủy</button>
-    </div>
+      <div class="content_button" v-show="focused">
+        <button
+          :class="{ 'disabled-btn': selectCitys.length >= 0 }"
+          @click="focused = !focused"
+          class="button1"
+        >
+          Đồng ý
+        </button>
+        <button @click="focused = false" class="button2">Hủy</button>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { mapState, mapActions } from "vuex";
+
+import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
 
 export default {
   name: "MultiSelect",
@@ -65,29 +70,44 @@ export default {
     return {
       focused: false,
       show: true,
+      search: "",
     };
+  },
+  watch: {
+    search: {
+      handler(val) {
+        let newFilterCitys = [];
+        if (val !== "") {
+          newFilterCitys = this.citys.filter((data) =>
+            data.name.toLowerCase().includes(val.toLowerCase())
+          );
+        } else {
+          newFilterCitys = JSON.parse(JSON.stringify(this.citys));
+        }
+        this.setFilterCitys(newFilterCitys);
+      },
+      immediate: true,
+    },
+  },
+  async created() {
+    await this.getListCity();
   },
 
   computed: {
     ...mapState({
-      filterCitys: (state) => state.citys,
       selectCitys: (state) => state.selectCitys,
     }),
-    search: {
-      get() {
-        return this.$store.getters.getSearch;
-      },
-      set(value) {
-        this.$store.dispatch("filterSearch", value);
-      },
-    },
+    ...mapGetters({
+      citys: "getCitys",
+      filterCitys: "getFilterCitys",
+    }),
   },
 
-  mounted() {
-    this.$store.dispatch("getCitys");
-  },
+  mounted() {},
 
   methods: {
+    ...mapActions({ getListCity: "getCitys" }),
+    ...mapMutations({ setFilterCitys: "setFilterCitys" }),
     handleClick() {
       this.focused = !this.focused;
     },
@@ -95,6 +115,9 @@ export default {
       addSelectField: "addSelectField",
       removeSelectField: "removeSelectField",
     }),
+    handleInput(e) {
+      this.search = e.target.value
+    },
     selectedCitys(e, city) {
       if (e.target.checked) {
         this.addSelectField(city);
@@ -109,6 +132,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
